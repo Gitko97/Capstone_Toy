297698a10ad39f46e2503912ffa775063736323a
Gitko97 <xcvdsf8216@gmail.com>
2021-08-17 18:16:09 +0900

    url 추가

--- toy_finder/finder/views.py
+++ toy_finder/finder/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
-
 # Create your views here.
+
+from rest_framework import views
+from ..toy_finder import wsgi
+from ..toy_finder import toy
+
+class ToyFinderView(views.APIView):
+    def get(self, request, image_url):
+        print("Get Call!")
+        print(image_url)
+
+    def post(self, request, image_url):
+        print("Post Call!")
+        print(image_url)
+
