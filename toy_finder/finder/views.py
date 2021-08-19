aca634fb12d4c98aa0bedc6fc845c78b642af18d
Gitko97 <xcvdsf8216@gmail.com>
2021-08-19 17:29:50 +0900

    결과를 json으로 받을 수 있게 변경

--- toy_finder/finder/views.py
+++ toy_finder/finder/views.py
@@ -1,20 +1,22 @@
 # Create your views here.
+import json
+import sys
+
 from django.http import HttpResponse
 from rest_framework import views
-import sys
+
 sys.path.append("..")
 
 from toy_finder.toyFinder import wsgi
 from toy_finder.toyFinder import toy
 
+
 class ToyFinderView(views.APIView):
     def get(self, request):
-        print(request.GET["image_url"])
         image_url = request.GET["image_url"]
-        print("Get Call!")
-        # print(image_url)
         scores = toy.get_similar_products_uri(wsgi.storage_bucket_name, wsgi.storage_region, wsgi.product_setID, wsgi.product_category, image_url, filter="")
-        return HttpResponse("성공 : " + "image_url")
+        scores_json = json.dumps([score.__dict__ for score in scores])
+        return HttpResponse(scores_json, content_type="text/json-comment-filtered")
 
     def post(self, request, image_url):
         print("Post Call!")
