4bc2e4456c0973dcc039c525ee358c780bce86a7
Gitko97 <xcvdsf8216@gmail.com>
2021-08-19 12:23:57 +0900

    vsion api 호출 및 결과를 가져오는 간단한 rest api 생성

--- toy_finder/finder/urls.py
+++ toy_finder/finder/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
-
 from . import views
 
 urlpatterns = [
-    path('<str:image_url>', views.ToyFinderView.as_view(), name='finder'),
+    path('api/', views.ToyFinderView.as_view(), name='image_url'),
 ]
--- toy_finder/finder/views.py
+++ toy_finder/finder/views.py
@@ -1,13 +1,20 @@
 # Create your views here.
-
+from django.http import HttpResponse
 from rest_framework import views
-from ..toy_finder import wsgi
-from ..toy_finder import toy
+import sys
+sys.path.append("..")
+
+from toy_finder.toyFinder import wsgi
+from toy_finder.toyFinder import toy
 
 class ToyFinderView(views.APIView):
-    def get(self, request, image_url):
+    def get(self, request):
+        print(request.GET["image_url"])
+        image_url = request.GET["image_url"]
         print("Get Call!")
-        print(image_url)
+        # print(image_url)
+        scores = toy.get_similar_products_uri(wsgi.storage_bucket_name, wsgi.storage_region, wsgi.product_setID, wsgi.product_category, image_url, filter="")
+        return HttpResponse("성공 : " + "image_url")
 
     def post(self, request, image_url):
         print("Post Call!")
