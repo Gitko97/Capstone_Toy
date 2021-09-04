ec13161abf840ad26da25821565fe49677196c1c
junghwan11 <sk971001@gmail.com>
2021-09-05 00:54:06 +0900

    create User Entity

---
+++ toy_springboot/src/main/java/com/test/toy_springboot/user/domain/User.java
@@ -0,0 +1,50 @@
+package com.test.toy_springboot.user.domain;
+
+import com.test.toy_springboot.user.dto.SignUpRequest;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long index;
+
+    @Column(nullable = false)
+    private String userId;
+
+    @Column(nullable = false)
+    private String pw;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String phoneNumber;
+
+    @CreatedDate
+    private LocalDateTime createdDate;
+
+    @LastModifiedDate
+    private LocalDateTime modifiedDate;
+
+    @Builder
+    public User(String userId, String pw, String name, String phoneNumber) {
+        this.userId = userId;
+        this.pw = pw;
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+    }
+
+
+
+
+}
