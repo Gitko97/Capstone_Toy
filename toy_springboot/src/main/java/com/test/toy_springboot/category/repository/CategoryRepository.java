b5b2a4d3764c5b474040275a1a1a38a58ef590cd
Gitko97 <xcvdsf8216@gmail.com>
2021-09-20 23:48:43 +0900

    Repository 어노테이션 추가

--- toy_springboot/src/main/java/com/test/toy_springboot/category/repository/CategoryRepository.java
+++ toy_springboot/src/main/java/com/test/toy_springboot/category/repository/CategoryRepository.java
@@ -2,6 +2,8 @@ package com.test.toy_springboot.category.repository;
 
 import com.test.toy_springboot.category.domain.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
 }
--- toy_springboot/src/main/java/com/test/toy_springboot/shop/repository/ShopRepository.java
+++ toy_springboot/src/main/java/com/test/toy_springboot/shop/repository/ShopRepository.java
@@ -2,6 +2,8 @@ package com.test.toy_springboot.shop.repository;
 
 import com.test.toy_springboot.shop.domain.Shop;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ShopRepository extends JpaRepository<Shop, Long> {
 }
