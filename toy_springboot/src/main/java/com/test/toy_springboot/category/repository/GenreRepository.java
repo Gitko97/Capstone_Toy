8a5ce5b78c4210d4296076cf7110f18d46adc89e
Gitko97 <xcvdsf8216@gmail.com>
2021-09-29 22:36:11 +0900

    캐릭터, 장르 오류 수정

--- toy_springboot/src/main/java/com/test/toy_springboot/category/repository/GenreRepository.java
+++ toy_springboot/src/main/java/com/test/toy_springboot/category/repository/GenreRepository.java
@@ -1,6 +1,5 @@
 package com.test.toy_springboot.category.repository;
 
-import com.test.toy_springboot.category.domain.Category;
 import com.test.toy_springboot.category.domain.Genre;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
