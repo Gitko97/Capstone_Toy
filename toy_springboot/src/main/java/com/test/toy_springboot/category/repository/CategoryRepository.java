1a066f3113e2f26659d5ce23a77dbee8ec5a763e
Gitko97 <xcvdsf8216@gmail.com>
2021-09-29 22:35:52 +0900

    Category 클래스 제거

--- toy_springboot/src/main/java/com/test/toy_springboot/category/repository/CategoryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.test.toy_springboot.category.repository;
-
-import com.test.toy_springboot.category.domain.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CategoryRepository extends JpaRepository<Category, Long> {
-}
