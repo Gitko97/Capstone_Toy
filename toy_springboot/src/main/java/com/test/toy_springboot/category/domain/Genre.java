6ef19f720b9e6d0f2d70597726c55025e3645bf0
Gitko97 <xcvdsf8216@gmail.com>
2021-09-29 22:33:31 +0900

    캐릭터, 장르 클래스 및 jpa 추가

---
+++ toy_springboot/src/main/java/com/test/toy_springboot/category/domain/Genre.java
@@ -0,0 +1,25 @@
+package com.test.toy_springboot.category.domain;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.test.toy_springboot.toy.domain.Toy;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
+public class Genre {
+    @Id
+    @NonNull
+    private String name;
+
+    @OneToOne(mappedBy = "genre",fetch = FetchType.EAGER)
+    @JsonIgnore
+    private Toy toy;
+}
---
+++ toy_springboot/src/main/java/com/test/toy_springboot/category/repository/GenreRepository.java
@@ -0,0 +1,10 @@
+package com.test.toy_springboot.category.repository;
+
+import com.test.toy_springboot.category.domain.Category;
+import com.test.toy_springboot.category.domain.Genre;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface GenreRepository extends JpaRepository<Genre, String> {
+}
