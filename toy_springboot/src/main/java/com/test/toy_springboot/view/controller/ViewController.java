13bf7e33ac375baf811f61b3d62443b0a7eb9966
Jeonghoon625 <Dr.kim1997@gmail.com>
2021-11-30 20:30:31 +0900

    홈 화면 완성

--- toy_springboot/src/main/java/com/test/toy_springboot/view/controller/ViewController.java
+++ toy_springboot/src/main/java/com/test/toy_springboot/view/controller/ViewController.java
@@ -1,14 +1,21 @@
 package com.test.toy_springboot.view.controller;
 
+import com.test.toy_springboot.shop.domain.Shop;
+import com.test.toy_springboot.toy.domain.Toy;
 import com.test.toy_springboot.user.domain.User;
 import com.test.toy_springboot.user.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Controller
 
@@ -36,12 +43,15 @@ public class ViewController {
         HttpSession session = request.getSession();
         String currentUserID = (String) session.getAttribute("currentUserID");
         if(currentUserID == null){
-            return "redirect:/signIn";
+            return "redirect:signIn";
         }
+        List<Toy> filteredToyList;
+
         String userId = currentUserID; // 토큰으로 Id 읽어오기
         User user = userService.getUserById(userId);
-
+        Shop shop = user.getShop(); //현재 접속중인 유저의 shop 읽어오기
         model.addAttribute("currentUser", user);
+        model.addAttribute("currentShop", shop);
         return "home";
     }
 
